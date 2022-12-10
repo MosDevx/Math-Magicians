e0cf535159119a92759fe521dffb8a53728f82e0
MosDevx <mosesk.wamae@gmail.com>
2022-12-10 13:46:47 +0300

    Made calculator-ui

---
+++ src/Components/Button.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import clsx from 'clsx'
+
+
+const styles = {
+	default: 'text-3xl p-4,  bg-slate-200 justify-center flex items-center border border-slate-300',
+	operator: 'bg-orange-400',
+	colSpan : 'col-span-2 w-full'
+}
+
+class Button extends React.Component{
+
+	constructor(props){
+		super(props)
+	}
+
+	render(
+		//destructer props
+	  {value,type} = this.props
+
+	){
+		return(
+		
+			<div className={clsx({[styles.default]:true, [styles.operator]:type === 'operator', [styles.colSpan]:value == 0}, )} >
+				{value}
+			</div>
+			
+		)
+	}
+}
+
+
+
+export default Button;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Calculator from './Components/Calculator'
+import Button from './Components/Button'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Calculator />
+    {/* <Button number={2} type={'operator'}/> */}
   </React.StrictMode>
 )
